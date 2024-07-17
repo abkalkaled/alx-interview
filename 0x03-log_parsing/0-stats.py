6632109fa499938582517ffb3574b3ca5e86e555
abkalkaled <aminukhaleedabiola@gmail.com>
2024-07-17 19:51:51 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,13 +3,29 @@ import random
 import sys
 from time import sleep
 import datetime
+import re
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+total_size = 0
+status_codes = {}
+
+def print_stats():
+    global total_size, status_codes
+    print(f"Total file size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        print(f"{code}: {status_codes[code]}")
+
+try:
+    count = 0
+    for line in sys.stdin:
+        count += 1
+        match = re.match(r"^(\S+) - \[(.*?)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)$", line)
+        if match:
+            ip, date, status, size = match.groups()
+            total_size += int(size)
+            status_codes[int(status)] = status_codes.get(int(status), 0) + 1
+        if count == 10:
+            print_stats()
+            count = 0
+except KeyboardInterrupt:
+    print_stats()
+
